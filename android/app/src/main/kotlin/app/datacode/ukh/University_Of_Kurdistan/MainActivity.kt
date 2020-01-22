fb5bb9fa878a819e8a73b92518a65b8608bc55d9
hooshyar <hooseyr@gmail.com>
2020-01-22 16:01:44 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/app/datacode/ukh/University_Of_Kurdistan/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.datacode.ukh.University_Of_Kurdistan
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
