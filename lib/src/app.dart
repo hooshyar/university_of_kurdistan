07273cf57d9c26e7ec3d12746d5cd16a1dbe08a7
hooshyar <hooseyr@gmail.com>
2020-01-23 10:36:09 +0300

    Initial Setup Folders Styling

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainAppScreen extends StatefulWidget {
+  @override
+  _MainAppScreenState createState() => _MainAppScreenState();
+}
+
+class _MainAppScreenState extends State<MainAppScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('hello')),
+    );
+  }
+}
